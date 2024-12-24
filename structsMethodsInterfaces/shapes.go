e0f5e36c663f69d73320b8222209a3eae36299c3
PJ Teh <pj.teh@foodpanda.com>
2024-12-24 15:17:52 +0800

    added Circle struct + Area methods. passing tests.

--- structsMethodsInterfaces/shapes.go
+++ structsMethodsInterfaces/shapes.go
@@ -5,12 +5,22 @@ type Rectangle struct {
 	Length float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
+const pi = 3.14159
+
 // Calculates perimeter of a rectangle of length l and width w
 func Perimeter(r Rectangle) float64 {
 	return 2*r.Length + 2*r.Width
 }
 
 // Calculates area of rectangle
-func Area(r Rectangle) float64 {
+func (rect *Rectangle) Area(r Rectangle) float64 {
 	return r.Length * r.Width
+}
+
+func (c *Circle) Area(circle Circle) float64 {
+	return pi * c.Radius * c.Radius
 }
--- structsMethodsInterfaces/shapes_test.go
+++ structsMethodsInterfaces/shapes_test.go
@@ -14,7 +14,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("test rectangular area", func(t *testing.T) {
 		rectangle := Rectangle{20.0, 40.0}
-		got := Area(rectangle)
+		got := rectangle.Area(rectangle)
 		want := 800.0
 
 		if got != want {
@@ -24,8 +24,8 @@ func TestArea(t *testing.T) {
 	// Test for circular area
 	t.Run("circular area", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
-		want := 314.15
+		got := circle.Area(circle)
+		want := 314.159
 
 		if got != want {
 			t.Errorf("want %.2f, got %.2f", want, got)
