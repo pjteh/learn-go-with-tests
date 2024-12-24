e5987a929adc747b150b803a8c2d7b960f18ce84
PJ Teh <pj.teh@foodpanda.com>
2024-12-24 11:36:48 +0800

    refactored sum.go and sum_test.go to take slice as args

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -1,6 +1,6 @@
 package arraysslices
 
-func Sum(arr [3]int) int {
+func Sum(arr []int) int {
 	sum := 0
 	for _, value := range arr {
 		sum += value
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -4,9 +4,17 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	t.Run("sum 3 element array", func(t *testing.T) {
-		array := [3]int{0, 10, 20}
+		array := []int{0, 10, 20}
 		got := Sum(array)
 		want := 30
+		if got != want {
+			t.Errorf("want %d but got %d", want, got)
+		}
+	})
+	t.Run("sum 7 element array", func(t *testing.T) {
+		array := []int{0, 1, 2, 3, 4, 5, 6}
+		got := Sum(array)
+		want := 21
 		if got != want {
 			t.Errorf("want %d but got %d", want, got)
 		}
