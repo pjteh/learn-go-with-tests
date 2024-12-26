f54d3a4a9e9d3c49179a9d414a5690697ca46830
PJ Teh <pj.teh@foodpanda.com>
2024-12-26 10:32:12 +0800

    maps: added error handling for unknown word search; passing tests.

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(searchString string) string {
-	return d[searchString]
+func (d Dictionary) Search(searchString string) (string, error) {
+	val, ok := d[searchString]
+	if !ok {
+		return "", fmt.Errorf("can't find '%s' in dictionary", searchString)
+	}
+	return val, nil
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is a test"}
-	got := dictionary.Search("test")
-	want := "this is a test"
-	assertHelper(t, got, want)
+	t.Run("known word search", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test"}
+		got, _ := dictionary.Search("test")
+		want := "this is a test"
+		assertHelper(t, got, want)
+	})
+	t.Run("unknown word search", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is a test"}
+		_, err := dictionary.Search("what?")
+		want := "can't find 'what?' in dictionary"
+
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+		assertHelper(t, err.Error(), want)
+	})
 }
 
 func assertHelper(t *testing.T, got, want string) {
